735bab7c74aa1d9852e2b2cb473c06e42d010c95
ulemons <ulemons92@gmail.com>
2019-07-30 00:19:26 +0200

    auto deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/dist/beautifulflower'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/beautifulflower/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
