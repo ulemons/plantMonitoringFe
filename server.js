2f17c1d8eda8d26095c753703caa74f073517de6
ulemons <ulemons92@gmail.com>
2019-07-29 19:53:15 +0200

    auto deploy

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/beautifulflower'));
 
+app.get('*', (req, res) => {
+    res.sendFile(`./dist/beautifulflower`); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/beautifulflower/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/beautifulflower/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
